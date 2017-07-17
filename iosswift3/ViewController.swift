d50b825cbae658a9d10cb38cef6dc510e8b94a2c
jostein <jostein@MacBook-Pro.local>
2017-07-17 23:46:15 +0800

    Initial Commit

---
+++ iosswift3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iosswift3
+//
+//  Created by jostein on 17/07/2017.
+//  Copyright © 2017 jostein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
